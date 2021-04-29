c91818a2c0dd8d8587aa9589b1375f69ec44f124
Ro Bullingdon <176murrayst@gmail.com>
2021-04-29 10:46:30 +0100

    sorting

--- foodswipe-frontend/src/environments/environment.ts
+++ foodswipe-frontend/src/environments/environment.ts
@@ -1,20 +1,22 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const env = require
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export const environment = {
   production: false,
   firebase: {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: ,
-    measurementId: 
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
   }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
