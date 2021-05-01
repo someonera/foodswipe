9c9742e4207444953d2c23d1402b338da1eb923d
James Chamberlain <james@chamber-made.com>
2021-05-01 11:33:34 +0200

    test(frontend/app.component.spec.ts): remove title rendered test & remove karma packages & config

--- foodswipe-frontend/src/app/app.component.spec.ts
+++ foodswipe-frontend/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('foodswipe-frontend');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('foodswipe-frontend app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('foodswipe-frontend app is running!');
+  // });
 });
