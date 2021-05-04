fe56059f57f381b1490aeeac1532e1542672757b
James Chamberlain <james@chamber-made.com>
2021-05-04 11:49:12 +0200

    test(frontend/dashboard.component.spec.ts): add tests for meal item buttons

--- foodswipe-frontend/src/app/dashboard/dashboard.component.spec.ts
+++ foodswipe-frontend/src/app/dashboard/dashboard.component.spec.ts
@@ -19,13 +19,9 @@ describe('DashboardComponent', () => {
   let authService: AuthService
   let mealsService: MealsService
 
-
-  beforeEach( async () => {
+  beforeAll( async () => {
     mealsService = createMockWithValues(MealsService, {mealList$: new BehaviorSubject<Meal[]>([])})
     authService = createMockWithValues(AuthService, {restaurant$: new BehaviorSubject<Restaurant>({id: 1, name: 'Casa del Ro', email: 'test@email.com'} as Restaurant)})
-  })
-
-  test('should render', async () => {
     await render(DashboardComponent, {
       imports: [RouterTestingModule],
       providers: [{
@@ -37,29 +33,52 @@ describe('DashboardComponent', () => {
         useValue: authService
       }]
     })
+  })
+
+  test('should render', async () => {
     expect(DashboardComponent).toBeTruthy();
   });
 
   test('navigate to orders on pressing Start Service', async () => {
+    expect(screen.getByRole('button', {name: /start service/i})).toHaveAttribute('ng-reflect-router-link','orders')
+  })
+
+  test('navigate to editmeal/new on pressing new meal', () => {
+    expect(screen.getByRole('button', {name: /add meal/i})).toHaveAttribute('ng-reflect-router-link','editmeal,new')
+
+  })
+
+
+  test('navigate to editmeal/:id on pressing edit meal', async () => {
+
+    mealsService = createMockWithValues(MealsService, {mealList$: new BehaviorSubject<Meal[]>([
+      {id: 1, name: 'food', description: 'yum', price: 1.00, image_url: '', restaurant: {id: 1, name: 'Casa del Ro'}},
+      {id: 2, name: 'more food', description: 'yum', price: 1.00, image_url: '', restaurant: {id: 1, name: 'Casa del Ro'}},
+      {id: 3, name: 'even more food', description: 'yum', price: 1.00, image_url: '', restaurant: {id: 1, name: 'Casa del Ro'}}
+    ])})
 
     await render(DashboardComponent, {
       imports: [RouterTestingModule],
-      providers: [
-        {
-          provide: MealsService,
-          useValue: mealsService
-        },
-        {
-          provide: AuthService,
-          useValue: authService
-        }
-      ]
+      providers: [{
+        provide: MealsService,
+        useValue: mealsService
+      },
+      {
+        provide: AuthService,
+        useValue: authService
+      }]
     })
+    const editMealBtns = screen.getAllByRole('button', {name: /edit meal/i})
 
-    expect(screen.getByRole('button', {name: /start service/i})).toHaveAttribute('ng-reflect-router-link','orders')
-    // expect(screen.getByRole('button', {name: /Menu/i})).toHaveAttribute('ng-reflect-router-link','orders')
-    // expect(screen.getByRole('button', {name: /+/i})).toHaveAttribute('ng-reflect-router-link','orders')
+    expect(editMealBtns[0]).toHaveAttribute('ng-reflect-router-link','editmeal,1')
+    expect(editMealBtns[1]).toHaveAttribute('ng-reflect-router-link','editmeal,2')
+    expect(editMealBtns[2]).toHaveAttribute('ng-reflect-router-link','editmeal,3')
+    // expect(screen.getAllByRole('button', {name: /edit meal/i})).toHaveAttribute('ng-reflect-router-link','editmeal,')
+  })
 
+  test('should not have edit meal buttons when no meals', async () => {
+    const buttonArray = (screen.queryAllByRole('button', {name: /edit meal/i}))
+    expect(buttonArray.length === 0)
   })
 
 })
